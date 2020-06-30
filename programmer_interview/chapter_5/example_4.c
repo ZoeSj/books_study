ff1c704fdae0c85dc5ba33c83a9fe5d39511258f
盛佳 <chengjia@guanliyuandeMacBook-Air.local>
2020-06-30 22:04:15 +0800

    example 4

---
+++ programmer_interview/chapter_5/example_4.c
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int a,x;
+	for (a=0,x=0; a <= 1 && !x++; a++)
+	{
+		/* code */
+		a++;
+	}
+	cout << a<< x<<endl;
+	return 0;
+}
